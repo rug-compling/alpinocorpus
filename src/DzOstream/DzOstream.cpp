b67e54e16167d0987a6addfce62314641e531f4c
Daniel de Kok <me@danieldk.eu>
2010-07-20 10:14:42 +0200

    Remove DzOstream/DzOstremBuf, they are too platform-dependent.

--- src/DzOstream/DzOstream.cpp
+++
@@ -1,7 +0,0 @@
-#include "DzOstream.ih"
-
-DzOstream::DzOstream(char const *filename) : std::ostream(0)
-{
-        d_streamBuf = QSharedPointer<DzOstreamBuf>(new DzOstreamBuf(filename));
-        rdbuf(d_streamBuf.data());
-}
