fff13516ac529722dd836fd4f83014ffd12af924
Daniel de Kok <me@danieldk.eu>
2010-08-02 10:20:30 +0200

    Recurse through directory-based corpora (allowing for subcorpora).

--- src/DirectoryCorpusReader/DirectoryCorpusReader.cpp
+++ src/DirectoryCorpusReader/DirectoryCorpusReader.cpp
@@ -8,13 +8,20 @@ DirectoryCorpusReader::DirectoryCorpusReader(QString const &directory) :
         throw runtime_error("DirectoryCorpusReader::DirectoryCorpusReader: Could not read directory entries!");
 
     // Retrieve and sort directory entries.
-    QStringList entries(dir.entryList());
+    QDirIterator entryIter(dir, QDirIterator::Subdirectories | QDirIterator::FollowSymlinks);
     QVector<IndexNamePair> indexedEntries;
-    copy(entries.constBegin(), entries.constEnd(), back_inserter(indexedEntries));
+    while (entryIter.hasNext()) {
+        QString entry = entryIter.next();
+        entry = entry.remove(0, d_directory.length());
+        if (entry[0] == '/')
+            entry.remove(0, 1);
+        indexedEntries.push_back(entry);
+    }
+
     sort(indexedEntries.begin(), indexedEntries.end(), IndexNamePairCompare());
 
     for (QVector<IndexNamePair>::const_iterator iter = indexedEntries.constBegin();
-    iter != indexedEntries.constEnd(); ++iter)
+            iter != indexedEntries.constEnd(); ++iter)
         d_entries.push_back(iter->name);
 }
 
