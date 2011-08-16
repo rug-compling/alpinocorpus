58ad72d282007f9a67f8f82adddb3a1cb10f78b8
Daniël de Kok <me@danieldk.eu>
2011-08-16 22:09:27 +0200

    Add the '-r' option to read an entry.
    Also report errors in the ProgramOptions constructor.

--- util/act/main.cpp
+++ util/act/main.cpp
@@ -35,6 +35,12 @@ void listCorpus(std::string const &treebank, std::string const &query)
   std::copy(i, end, std::ostream_iterator<std::string>(std::cout, "\n"));  
 }
 
+void readEntry(std::string const &treebank, std::string const &entry)
+{
+  boost::scoped_ptr<CorpusReader> rd(CorpusReader::open(treebank));
+  std::cout << rd->read(entry);
+}
+
 void usage(std::string const &programName)
 {
     std::cerr << "Usage: " << programName << " [OPTION] treebank" <<
@@ -69,50 +75,74 @@ void writeDactCorpus(std::string const &treebank, std::string const &treebankOut
 
 int main(int argc, char *argv[])
 {
-  ProgramOptions opts(argc, const_cast<char const **>(argv), "c:lq:");
-  
-  if (opts.arguments().size() != 1)
-  {
-    usage(opts.programName());
+  boost::scoped_ptr<ProgramOptions> opts;
+  try {
+    opts.reset(new ProgramOptions(argc, const_cast<char const **>(argv),
+      "c:lq:r:"));
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
     return 1;
   }
   
-  if (opts.option('c') && opts.option('l')) {
-    std::cerr << opts.programName() <<
-      ": the '-c' and '-l' options cannot be used simultaneously." <<
+  if (opts->arguments().size() != 1)
+  {
+    usage(opts->programName());
+    return 1;
+  }
+
+  size_t cmdCount = 0;
+  char const commands[] = "clr";
+  for (size_t i = 0; i < 3; ++i)
+    if (opts->option(commands[i]))
+      ++cmdCount;
+  
+  if (cmdCount > 1) {
+    std::cerr << opts->programName() <<
+      ": the '-c', '-g', and '-r' options cannot be used simultaneously." <<
       std::endl;
     return 1;
   }
   
-  if (!opts.option('c') && !opts.option('l')) {
-    std::cerr << opts.programName() <<
-    ": either the '-c' or '-l' option should be used." <<
+  if (cmdCount == 0) {
+    std::cerr << opts->programName() <<
+    ": one of the '-c', '-g' or '-r' option should be used." <<
     std::endl;
     return 1;
   }
   
   std::string query;
-  if (opts.option('q'))
-    query = opts.optionValue('q');  
+  if (opts->option('q'))
+    query = opts->optionValue('q');  
   
-  if (opts.option('c')) {
+  if (opts->option('c')) {
     try {
-        std::string treebank = opts.arguments().at(0);
-        std::string treebankOut = opts.optionValue('c').c_str();
+        std::string treebank = opts->arguments().at(0);
+        std::string treebankOut = opts->optionValue('c').c_str();
       writeDactCorpus(treebank, treebankOut, query);
     } catch (std::runtime_error const &e) {
-        std::cerr << opts.programName() <<
+        std::cerr << opts->programName() <<
         ": error creating Dact treebank: " << e.what() << std::endl;
         return 1;
     }
   }
-  
-  if (opts.option('l')) {
+
+  if (opts->option('r')) {
     try {
-        std::string treebank = opts.arguments().at(0).c_str();
+      std::string treebank = opts->arguments().at(0).c_str();
+      readEntry(treebank, opts->optionValue('r'));
+    } catch (std::runtime_error const &e) {
+        std::cerr << opts->programName() <<
+        ": error reading entry: " << e.what() << std::endl;
+        return 1;
+    }    
+  }
+  
+  if (opts->option('l')) {
+    try {
+        std::string treebank = opts->arguments().at(0).c_str();
         listCorpus(treebank, query);
     } catch (std::runtime_error const &e) {
-        std::cerr << opts.programName() <<
+        std::cerr << opts->programName() <<
         ": error listing treebank: " << e.what() << std::endl;
         return 1;
     }    
