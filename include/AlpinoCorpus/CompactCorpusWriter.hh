93eb4c2a4a6b0932422c7543e14f34bc41f07fa9
Daniël de Kok <me@danieldk.eu>
2011-10-03 13:15:48 +0200

    CompactCorpusWriter implementation behind a d-pointer.

--- include/AlpinoCorpus/CompactCorpusWriter.hh
+++ include/AlpinoCorpus/CompactCorpusWriter.hh
@@ -1,70 +1,29 @@
-#ifndef ALPINO_COMPACT_CORPUSWRITER_HH
-#define ALPINO_COMPACT_CORPUSWRITER_HH
+#ifndef ALPINOCORPUS_COMPACT_CORPUS_WRITER
+#define ALPINOCORPUS_COMPACT_CORPUS_WRITER
 
-#include <iostream>
-
-#include <tr1/memory>
-
-#include <boost/config.hpp>
-
-#if defined(BOOST_HAS_THREADS)
-#include <boost/thread/mutex.hpp>
-#endif
+#include <string>
 
 #include <AlpinoCorpus/CorpusReader.hh>
 #include <AlpinoCorpus/CorpusWriter.hh>
 
-namespace alpinocorpus
-{
+namespace alpinocorpus {
 
-typedef std::tr1::shared_ptr<std::ostream> ostreamPtr;
+class CompactCorpusWriterPrivate;
 
 class CompactCorpusWriter : public CorpusWriter
 {
-    struct NullStream : std::ostream
-    {
-        NullStream() : std::ios(0), std::ostream(0) {}
-    };
-
 public:
-	CompactCorpusWriter(std::string const &basename);
-	CompactCorpusWriter(ostreamPtr dataStream, ostreamPtr indexStream) :
-		d_dataStream(dataStream), d_indexStream(indexStream), d_offset(0) {}
-	CompactCorpusWriter() :
-		d_dataStream(new NullStream), d_indexStream(new NullStream), d_offset(0) {}
-	CompactCorpusWriter(CompactCorpusWriter const &other);
-	CompactCorpusWriter &operator=(CompactCorpusWriter const &other);
+    CompactCorpusWriter(std::string const &basename);
+    virtual ~CompactCorpusWriter();
+
 protected:
-	void writeEntry(std::string const &name, std::string const &data);
-	void writeEntry(std::string const &name, char const *buf, size_t len);
-    void writeEntry(CorpusReader const &corpus, bool fail_first);
+    void writeEntry(std::string const &name, std::string const &content);
+    void writeEntry(CorpusReader const &corpus, bool failsafe = false);
+
 private:
-	void copy(CompactCorpusWriter const &other);
-    void writeFailFirst(CorpusReader const &corpus);
-    void writeFailSafe(CorpusReader const &corpus);
-
-	ostreamPtr d_dataStream;
-	ostreamPtr d_indexStream;
-	size_t d_offset;
-
-#if defined(BOOST_HAS_THREADS)
-    mutable boost::mutex d_writeMutex;
-#endif
+    CompactCorpusWriterPrivate *d_private;
 };
 
-inline CompactCorpusWriter::CompactCorpusWriter(CompactCorpusWriter const &other)
-{
-	copy(other);
 }
 
-inline CompactCorpusWriter &CompactCorpusWriter::operator=(CompactCorpusWriter const &other)
-{
-	if (this != &other)
-		copy(other);
-	
-	return *this;
-}
-
-}
-
-#endif  // ALPINO_INDEXED_CORPUSWRITER_HH
+#endif // ALPINOCORPUS_COMPACT_CORPUS_WRITER
--- src/CompactCorpusWriter.cpp
+++ src/CompactCorpusWriter.cpp
@@ -1,98 +1,31 @@
-#include <fstream>
 #include <string>
 
-#include <boost/config.hpp>
-
-#if defined(BOOST_HAS_THREADS)
-#include <boost/thread/mutex.hpp>
-#endif
-
-#include <AlpinoCorpus/CorpusReader.hh>
-#include <AlpinoCorpus/Error.hh>
 #include <AlpinoCorpus/CompactCorpusWriter.hh>
-#include <util/base64.hh>
+#include <AlpinoCorpus/CorpusReader.hh>
+#include <AlpinoCorpus/CorpusWriter.hh>
 
-#include "DzOstream.hh"
-
-using namespace std;
+#include "CompactCorpusWriterPrivate.hh"
 
 namespace alpinocorpus {
 
-
 CompactCorpusWriter::CompactCorpusWriter(std::string const &basename) :
-	d_offset(0)
+    d_private(new CompactCorpusWriterPrivate(basename))
+{}
+
+CompactCorpusWriter::~CompactCorpusWriter()
 {
-	std::string dataFilename = basename + ".data.dz";
-	d_dataStream.reset(new DzOstream(dataFilename.c_str()));
-	if (!d_dataStream)
-		throw OpenError(dataFilename, "Could not open file for writing");
-
-	std::string indexFilename = basename + ".index";
-	d_indexStream.reset(new std::ofstream(indexFilename.c_str()));
-	if (!d_indexStream)
-		throw OpenError(indexFilename, "Could not open file for writing");
+    delete d_private;
 }
 
-void CompactCorpusWriter::copy(CompactCorpusWriter const &other)
+void CompactCorpusWriter::writeEntry(std::string const &name, std::string const &content)
 {
-	d_dataStream = other.d_dataStream;
-	d_indexStream = other.d_indexStream;
-	d_offset = other.d_offset;
+    d_private->writeEntry(name, content);
 }
 
-void CompactCorpusWriter::writeEntry(std::string const &name, std::string const &data)
+void CompactCorpusWriter::writeEntry(CorpusReader const &corpus, bool failsafe)
 {
-#if defined(BOOST_HAS_THREADS)
-    boost::mutex::scoped_lock lock(d_writeMutex);
-#endif
-
-	*d_dataStream << data;
-	*d_indexStream << name << "\t" << util::b64_encode(d_offset) << "\t" <<
-		util::b64_encode(data.size()) << endl;
-	d_offset += data.size();
+    d_private->writeEntry(corpus, failsafe);
 }
 
-void CompactCorpusWriter::writeEntry(std::string const &name, char const *buf, size_t len)
-{
-#if defined(BOOST_HAS_THREADS)
-    boost::mutex::scoped_lock lock(d_writeMutex);
-#endif
 
-	d_dataStream->write(buf, len);
-	*d_indexStream << name << "\t" << util::b64_encode(d_offset) << "\t" <<
-		util::b64_encode(len) << endl;
-	d_offset += len;
 }
-
-void CompactCorpusWriter::writeEntry(CorpusReader const &corpus, bool fail_first)
-{    
-    if (fail_first)
-        writeFailFirst(corpus);
-    else
-        writeFailSafe(corpus);
-}
-
-void CompactCorpusWriter::writeFailFirst(CorpusReader const &corpus)
-{
-    for (CorpusReader::EntryIterator i(corpus.begin()), end(corpus.end());
-         i != end; ++i)
-        write(*i, corpus.read(*i));
-}
-
-void CompactCorpusWriter::writeFailSafe(CorpusReader const &corpus)
-{
-    BatchError err;
-
-    for (CorpusReader::EntryIterator i(corpus.begin()), end(corpus.end());
-         i != end; ++i)
-        try {
-            write(*i, corpus.read(*i));
-        } catch (Error const &e) {
-            err.append(e);
-        }
-
-    if (!err.empty())
-        throw err;
-}
-
-}   // namespace alpinocorpus
