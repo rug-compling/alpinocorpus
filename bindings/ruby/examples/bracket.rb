3e28873f3eaf0bec1823415bb15c3ec6a83ba855
Daniël de Kok <me@danieldk.eu>
2011-10-25 20:13:19 +0200

    Ruby example: sentence bracketing.

---
+++ bindings/ruby/examples/bracket.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'alpinocorpus'
+require 'rexml/document'
+require 'xml/xslt'
+
+if ARGV.length != 2 then
+  puts "#{$0} query corpus"
+  exit
+end
+
+query = ARGV[0]
+corpus = ARGV[1]
+
+styledir = File.dirname($0)
+stylesheet = "#{styledir}/bracketed-sentence.xsl"
+
+xslt = XML::XSLT.new()
+xslt.xsl = REXML::Document.new(File.new(stylesheet))
+
+reader = CorpusReader.new(corpus)
+
+markers = [MarkerQuery.new(query, "active", "1")]
+
+reader.query(query).each { |e|
+  xslt.xml = REXML::Document.new(reader.read(e, markers))
+  puts xslt.serve()
+}
