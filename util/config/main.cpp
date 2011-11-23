08a124107753a97af021066ec295f19c66d890bf
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2011-11-23 17:30:13 +0100

    alpinocorpus-config: rewrite to make it use command line options

--- util/config/main.cpp
+++ util/config/main.cpp
@@ -1,28 +1,48 @@
-/*! \todo command line options:
- *  - --prefix
- *  - --version
- *  - --ldflags
- *  - --cflags
- * etc.
- */
-
 #include <AlpinoCorpus/Config.hh>
 #include <iostream>
+#include <cstring>
 
 using std::cout;
 using std::endl;
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc != 2) {
+        cout << "Usage: " << argv[0] << " OPTION" << endl
+             << "Known values for OPTION are:" << endl
+             << endl
+             << "  --version         output version information" << endl
+             << "  --versionmajor    output major version number" << endl
+             << "  --versionminor    output minor version number" << endl
+             << "  --options         output list of enabled options" << endl;
+        return 0;
+    }
+
     alpinocorpus::Config cfg;
 
-    cout << endl
-
-         << "Alpino Corpus version " << cfg.Version() << endl
-         << "version major number :  " << cfg.VersionMajor() << endl
-         << "version minor number :  " << cfg.VersionMinor() << endl
-         << endl
-
-         << "with Berkeley DB XML :  " << (cfg.WithDBXML() ? "yes" : "no") << endl
-         << endl;
+    if (! strcmp (argv [1], "--version"))
+        cout << cfg.Version() << endl;
+    else if (! strcmp (argv [1], "--versionmajor"))
+        cout << cfg.VersionMajor() << endl;
+    else if (! strcmp (argv [1], "--versionminor"))
+        cout << cfg.VersionMinor() << endl;
+    else if (! strcmp (argv [1], "--options")) {
+        char *p = "";
+        if (cfg.WithDBXML()) {
+            cout << p << "with-dbxml";
+            p = " ";
+        }
+        /*
+        if (cfg.WithSSL()) {
+            cout << p << "with-ssl";
+            p = " ";
+        }
+        if (cfg.WithSSLStrict()) {
+            cout << p << "with-ssl-strict";
+            p = " ";
+        }
+        */
+        cout << endl;
+    } else
+        cout << "Invalid arggument: " << argv[1];
 }
