0cdfd4a06781de6d24c6cdcfab9035d07b9207b0
Daniël de Kok <me@danieldk.eu>
2012-08-23 12:58:32 +0200

    Add missing header.

---
+++ include/AlpinoCorpus/Entry.hh
@@ -0,0 +1,16 @@
+#ifndef ALPINOCORPUS_ENTRY_HH
+#define ALPINOCORPUS_ENTRY_HH
+
+#include <string>
+
+namespace alpinocorpus
+{
+
+struct Entry {
+  std::string name;
+  std::string contents;
+};
+
+}
+
+#endif
