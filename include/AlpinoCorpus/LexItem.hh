654198f1abc3d8b16dc1a4c54c4a26c972b2d707
Daniël de Kok <me@danieldk.eu>
2012-09-12 15:36:08 +0200

    Add missing LexItem.hh.

---
+++ include/AlpinoCorpus/LexItem.hh
@@ -0,0 +1,30 @@
+#ifndef ALPINOCORPUS_LEX_ITEM_HH
+#define ALPINOCORPUS_LEX_ITEM_HH
+
+
+#include <string>
+#include <set>
+#include <vector>
+
+namespace alpinocorpus {
+
+struct LexItem
+{
+    std::string word;
+    size_t begin;
+    std::set<size_t> matches;
+
+    inline bool operator<(LexItem const &other) const
+    {
+        if (begin != other.begin)
+          return begin < other.begin;
+        else
+          return word < other.word;
+    }
+};
+
+static std::vector<LexItem> *parseSentence(std::string const &sentence);
+
+}
+
+#endif // ALPINOCORPUS_LEX_ITEM_HH
