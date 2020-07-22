b7e503991dc06fb33635024bfc1178096638a020
Daniël de Kok <me@danieldk.eu>
2020-07-22 08:55:08 +0200

    Fix warnings in FindXercesC/FindXQilla

--- cmake/FindXercesC.cmake
+++ cmake/FindXercesC.cmake
@@ -5,7 +5,7 @@ find_path(XERCESC_INCLUDE_DIR NAMES xercesc/parsers/SAXParser.hpp)
 find_library(XERCESC_LIBRARY NAMES xerces-c)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(XERCESC DEFAULT_MSG
+find_package_handle_standard_args(XercesC DEFAULT_MSG
   XERCESC_INCLUDE_DIR XERCESC_LIBRARY)
 
 mark_as_advanced(XERCESC_INCLUDE_DIR XERCESC_LIBRARY)
