9996fcf05505d58451477a0a84938f05fbd429ac
Daniël de Kok <me@danieldk.eu>
2020-12-04 10:33:53 +0100

    Make this repository a Nix flake

--- default.nix
+++ default.nix
@@ -1,41 +1,7 @@
-{
-  pkgs ? import <nixpkgs> {}
-}:
-
-with pkgs;
-
-stdenv.mkDerivation {
-  name = "alpinocorpus";
-
-  src = nix-gitignore.gitignoreSource [ ".git/" ] ./.;
-
-  nativeBuildInputs = [
-    meson
-    ninja
-    pkg-config
-  ];
-
-  buildInputs = [
-    boost
-    dbxml
-    libxml2
-    libxslt
-    xercesc
-    xqilla
-    zlib
-  ] ++ lib.optional stdenv.isDarwin libiconv;
-
-  doCheck = true;
-
-  # Meson is no longer able to pick up Boost automatically.
-  # https://github.com/NixOS/nixpkgs/issues/86131
-  BOOST_INCLUDEDIR = "${stdenv.lib.getDev boost}/include";
-  BOOST_LIBRARYDIR = "${stdenv.lib.getLib boost}/lib";
-
-  meta = with stdenv.lib; {
-    description = "Library for Alpino treebanks";
-    homepage = "https://github.com/rug-compling/alpinocorpus";
-    license = licenses.lgpl21;
-    platforms = platforms.unix;
-  }; 
-}
+(import (
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+    sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2"; }
+) {
+  src =  ./.;
+}).defaultNix
