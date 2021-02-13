c3d9d74b8f505c2c687a228b806f4137a121980c
Daniël de Kok <me@danieldk.eu>
2021-02-13 07:46:52 +0100

    flake: stdenv.lib -> lib

--- flake.nix
+++ flake.nix
@@ -38,10 +38,10 @@
 
       # Meson is no longer able to pick up Boost automatically.
       # https://github.com/NixOS/nixpkgs/issues/86131
-      BOOST_INCLUDEDIR = "${stdenv.lib.getDev boost}/include";
-      BOOST_LIBRARYDIR = "${stdenv.lib.getLib boost}/lib";
+      BOOST_INCLUDEDIR = "${lib.getDev boost}/include";
+      BOOST_LIBRARYDIR = "${lib.getLib boost}/lib";
 
-      meta = with stdenv.lib; {
+      meta = with lib; {
         description = "Library for Alpino treebanks";
         homepage = "https://github.com/rug-compling/alpinocorpus";
         license = licenses.lgpl21;
