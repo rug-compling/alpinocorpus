9b158bed0a186a14e1423565e73b8face729fac2
Daniël de Kok <me@danieldk.eu>
2021-04-29 20:36:31 +0200

    Parse XSL from a file rather than a string
    Fixes #56.

--- src/Stylesheet.cpp
+++ src/Stylesheet.cpp
@@ -14,71 +14,83 @@ extern "C" {
 #include <AlpinoCorpus/Error.hh>
 #include <AlpinoCorpus/Stylesheet.hh>
 
+namespace {
+    alpinocorpus::Stylesheet *readDoc(xmlDocPtr xslDoc) {
+        xsltStylesheetPtr xsl = xsltParseStylesheetDoc(xslDoc);
+        if (xsl == 0) {
+            xmlFreeDoc(xslDoc);
+            throw alpinocorpus::Error("Stylesheet::initWithStylesheet: Could not read stylesheet as XML data");
+        }
+
+        // The stylesheet has taken ownership of xslDoc, so we do not
+        // need to manage it separately.
+        std::shared_ptr<xsltStylesheet> xslPtr(xsl, xsltFreeStylesheet);
+
+        return new alpinocorpus::Stylesheet(xslPtr);
+    }
+
+}
+
 namespace alpinocorpus {
 
-Stylesheet::Stylesheet(std::string const &xsl)
-{
-    initWithStylesheet(xsl);
-}
+    Stylesheet *Stylesheet::readData(std::string const &data) {
+        xmlDocPtr xslDoc = xmlReadMemory(data.c_str(), data.size(), 0, 0,
+                                         XSLT_PARSE_OPTIONS);
+        if (xslDoc == 0) {
+            throw Error("Stylesheet::initWithStylesheet: Could not read stylesheet as XML data");
+        }
 
-Stylesheet::~Stylesheet()
-{
-}
-
-void Stylesheet::initWithStylesheet(std::string const &xsl)
-{
-    xmlDocPtr xslDoc = xmlReadMemory(xsl.c_str(), xsl.size(), 0, 0,
-        XSLT_PARSE_OPTIONS);
-
-    if (xslDoc == 0)
-    {
-      throw Error("Stylesheet::initWithStylesheet: Could not read stylesheet as XML data");
+        return ::readDoc(xslDoc);
     }
 
-    d_xslPtr.reset(xsltParseStylesheetDoc(xslDoc), xsltFreeStylesheet);
+    Stylesheet *Stylesheet::readFile(std::string const &filename) {
+        xmlDocPtr xslDoc = xmlReadFile(filename.c_str(), 0, XSLT_PARSE_OPTIONS);
+        if (xslDoc == 0) {
+            throw Error("Stylesheet::initWithStylesheet: Could not read stylesheet as XML data");
+        }
 
-    if (!d_xslPtr)
-    {
-      throw Error("Stylesheet::initWithStylesheet: Could not read stylesheet as XML data");
+        return ::readDoc(xslDoc);
     }
-}
 
-std::string Stylesheet::transform(std::string const &xml) const
-{
-    // Read XML data intro an xmlDoc.
-    std::shared_ptr<xmlDoc> doc(
-        xmlReadMemory(xml.c_str(), xml.size(), 0, 0, 0),
-        xmlFreeDoc);
+    Stylesheet::~Stylesheet() {
+    }
 
-    if (!doc)
-        throw Error("Stylesheet::transform: Could not open XML data");
 
-    std::shared_ptr<xsltTransformContext> ctx(
-        xsltNewTransformContext(d_xslPtr.get(), doc.get()),
-        xsltFreeTransformContext);
-    xsltSetCtxtParseOptions(ctx.get(), XSLT_PARSE_OPTIONS);
+    std::string Stylesheet::transform(std::string const &xml) const {
+        // Read XML data intro an xmlDoc.
+        std::shared_ptr<xmlDoc> doc(
+                xmlReadMemory(xml.c_str(), xml.size(), 0, 0, 0),
+                xmlFreeDoc);
 
-    // Transform...
-    std::shared_ptr<xmlDoc> res(
-        xsltApplyStylesheetUser(d_xslPtr.get(), doc.get(), NULL, NULL, NULL, ctx.get()),
-        xmlFreeDoc);
+        if (!doc)
+            throw Error("Stylesheet::transform: Could not open XML data");
 
-    if (!res)
-        throw Error("Stylesheet::transform: Could not apply transformation!");
-    else if (ctx->state != XSLT_STATE_OK)
-        throw Error("Stylesheet::transform: Transformation error, check your query!");
+        std::shared_ptr<xsltTransformContext> ctx(
+                xsltNewTransformContext(d_xslPtr.get(), doc.get()),
+                xsltFreeTransformContext);
+        xsltSetCtxtParseOptions(ctx.get(), XSLT_PARSE_OPTIONS);
 
-    xmlChar *bareOutput = 0;
-    int outputLen = -1;
-    xsltSaveResultToString(&bareOutput, &outputLen, res.get(), d_xslPtr.get());
-    std::shared_ptr<xmlChar> output(bareOutput, xmlFree); 
+        // Transform...
+        std::shared_ptr<xmlDoc> res(
+                xsltApplyStylesheetUser(d_xslPtr.get(), doc.get(), NULL, NULL, NULL, ctx.get()),
+                xmlFreeDoc);
 
-    if (!output)
-      return std::string();
+        if (!res)
+            throw Error("Stylesheet::transform: Could not apply transformation!");
+        else if (ctx->state != XSLT_STATE_OK)
+            throw Error("Stylesheet::transform: Transformation error, check your query!");
 
-    std::string result(reinterpret_cast<char const *>(output.get()));
+        xmlChar *bareOutput = 0;
+        int outputLen = -1;
+        xsltSaveResultToString(&bareOutput, &outputLen, res.get(), d_xslPtr.get());
+        std::shared_ptr<xmlChar> output(bareOutput, xmlFree);
 
-    return result;
-}
+        if (!output)
+            return std::string();
+
+        std::string result(reinterpret_cast<char const *>(output.get()));
+
+        return result;
+    }
 
 }
